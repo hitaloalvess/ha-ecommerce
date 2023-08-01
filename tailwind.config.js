1973aa086970764b6da65b7b41de209ce684ea68
hitaloalvess <hitalo.ralves@outlook.com>
2023-08-01 20:42:29 -0300

    style: add color hex in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primaryRed': '#F0141E',
+        'primaryBlack': '#030303',
+        'primaryWhite': '#FFFFFF',
+        'customEmperor-600': '#515151',
+        'customNevada-500': '#717274',
+        'customAlto-300': '#D9D9D9',
+        'customEbony-950': '#060907',
+        'customBunker-950': '#080A0E',
+        'customBlackPearl-950': '#040C1B',
+        'customVulcan-950': '#0F141F',
+        'customMirage-950': '#151B26',
+        'customBigStone-950': '#1A2841'
+      }
+    },
   },
   plugins: [],
 }
